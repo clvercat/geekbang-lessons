aa3f43ed4fe66a5a5b99a786799e22563567623c
ltxlouis <ltxlouis@users.noreply.github.com>
2021-03-17 16:28:45 +0800

    add dependency

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # geekbang-lessons
 极客时间课程工程
+
+## 第三次作业
+
+```text
+需求一（必须）
+整合 https://jolokia.org/
+实现一个自定义 JMX MBean，通过 Jolokia 做 Servlet 代理
+需求二（选做）
+继续完成 Microprofile config API 中的实现
+扩展 org.eclipse.microprofile.config.spi.ConfigSource 实现，包括 OS 环境变量，以及本地配置文件
+扩展 org.eclipse.microprofile.config.spi.Converter 实现，提供 String 类型到简单类型
+通过 org.eclipse.microprofile.config.Config 读取当前应用名称
+应用名称 property name = “application.name”
+作业提交链接： https://jinshuju.net/f/EDwozM
+作业提交截至日期：3 月 17 日（含）前
+```
+
+打包
+
+```shell
+mvn -f projects/stage-0/user-platform/pom.xml clean package -U
+```
+
+运行
+
+```shell
+java -jar projects/stage-0/user-platform/user-web/target/user-web-v1-SNAPSHOT-war-exec.jar
+```
+
+http://localhost:8080
---
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/Jolokia/JolokiaDemo.java
@@ -0,0 +1,23 @@
+package org.geektimes.projects.user.Jolokia;
+
+import java.util.Map;
+import org.jolokia.client.J4pClient;
+import org.jolokia.client.request.J4pReadRequest;
+import org.jolokia.client.request.J4pReadResponse;
+
+public class JolokiaDemo {
+
+  public static void main(String[] args) throws Exception {
+    J4pClient j4pClient = new J4pClient("http://localhost:8080/jolokia");
+    J4pReadRequest req = new J4pReadRequest("java.lang:type=Memory",
+        "HeapMemoryUsage");
+    J4pReadResponse resp = j4pClient.execute(req);
+    Map<String, Long> vals = resp.getValue();
+    long used = vals.get("used");
+    long max = vals.get("max");
+    int usage = (int) (used * 100 / max);
+    System.out.println("Memory usage: used: " + used +
+        " / max: " + max + " = " + usage + "%");
+  }
+
+}
